f54397658eabe5c4ef6232643a6cbdbc23eb6086
Miguel Ibero <miguel@ibero.me>
2021-07-12 15:23:19 +0200

    fixed exception when weigths are empty

--- nnrecommend/operation.py
+++ nnrecommend/operation.py
@@ -296,11 +296,12 @@ class RunTracker:
 
         if hasattr(model, 'get_embedding_weight'):
             weight = model.get_embedding_weight()
-            if self.__embedding_epoch_num > 0 and epoch % self.__embedding_epoch_num == 0:
-                self.__tb.add_embedding(weight, global_step=epoch,
-                    metadata=self.__embedding_md,
-                    metadata_header=self.__embedding_md_header)
-            self.__tb.add_histogram("embedding", weight, global_step=epoch)
+            if weight is not None and len(weight) > 0:
+                if self.__embedding_epoch_num > 0 and epoch % self.__embedding_epoch_num == 0:
+                    self.__tb.add_embedding(weight, global_step=epoch,
+                        metadata=self.__embedding_md,
+                        metadata_header=self.__embedding_md_header)
+                self.__tb.add_histogram("embedding", weight, global_step=epoch)
         self.__tb.flush()
 
     def track_test_result(self, epoch: int, result: TestResult):
